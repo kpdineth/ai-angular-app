88b3fa2185291890beee00cf377af89fb7368dc6
Dineth <kpdineth@gmail.com>
2025-03-08 21:07:03 +0800

    show-text

---
+++ src/app/show-text/show-text.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-show-text',
+  standalone: true,
+  imports: [],
+  templateUrl: './show-text.component.html',
+  styleUrl: './show-text.component.css'
+})
+export class ShowTextComponent {
+
+}
